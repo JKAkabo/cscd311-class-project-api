30756a7ec5a0cda39cb3a7b1c025f2a0daffbcbb
Ken <developer.akabojohnkennedy@gmail.com>
2019-10-10 15:18:51 +0000

    Setup instructions

--- setup.js
+++ setup.js
@@ -2,16 +2,26 @@ const models = require('./models');
 const db = require('./db');
 
 let setupStudents = async (studentsData) => {
-    let students = [];
-    studentsData.forEach(studentData => {
+    studentsData.forEach(async studentData => {
+        console.log('Setting up student: ' + studentData.firstName + ' ' + studentData.lastName );
         try {
             let student = new models.Student(studentData);
             await student.save();
-            students.push(student);
         } catch (err) { console.log(err) }
     });
 }
-db.connect().then(() => {
+
+let setupRooms = async (roomsData) => {
+    roomsData.forEach(async roomData => {
+        console.log('Setting up room: ' + roomData.number);
+        try {
+            let room = new models.Room(roomData);
+            await room.save();
+        } catch (err) { console.log(err) }
+    })
+}
+
+db.connect().then(async () => {
     // CREATE STUDENTS
     const studentsData = [
         {
@@ -29,7 +39,7 @@ db.connect().then(() => {
             gender: 'M'
         },
         {
-            idNumber: '10664055',
+            idNumber: '10664058',
             pin: '12345',
             firstName: 'Natasha',
             lastName: 'Romanoff',
@@ -51,22 +61,83 @@ db.connect().then(() => {
         }
     ];
 
-    let students = [];
-    let halls = [];
+    let legonHall = new models.Hall({ name: 'Legon Hall' });
+    await legonHall.save();
 
-    const hallsData =  [
-        { name: 'Legon Hall' },
-        { name: 'Akuaffo Hall' },
-        { name: 'CommonWealth Hall' },
-        { name: 'Mensah Sarbah Hall' }
-    ];
+    let akuaffoHall = new models.Hall({ name: 'Akuaffo Hall' });
+    await akuaffoHall.save();
 
-    hallsData.forEach(hallData => {
-        try {
-            let hall = new models.Hall(hallData);
-            hall.save();
+    let voltaHall = new models.Hall({ name: 'Volta Hall' });
+    await voltaHall.save();
 
-            halls.push(hall);
-        } catch (err) { console.log(err) }
-    })
+    let commonwealthHall = new models.Hall({ name: 'Commonwealth Hall' });
+    await commonwealthHall.save();
+
+    const roomsData = [
+        {
+            number: 'AA406',
+            hallId: legonHall._id,
+            occupantGender: 'M'
+        },
+        {
+            number: 'AA407',
+            hallId: legonHall._id,
+            occupantGender: 'M'
+        },
+        {
+            number: 'AB406',
+            hallId: legonHall._id,
+            occupantGender: 'F'
+        },
+        {
+            number: 'AB407',
+            hallId: legonHall._id,
+            occupantGender: 'F'
+        },
+        {
+            number: 'AA201',
+            hallId: akuaffoHall._id,
+            occupantGender: 'F'
+        },
+        {
+            number: 'AA204',
+            hallId: akuaffoHall._id,
+            occupantGender: 'F'
+        },
+        {
+            number: 'AB304',
+            hallId: akuaffoHall._id,
+            occupantGender: 'M'
+        },
+        {
+            number: 'AB307',
+            hallId: akuaffoHall._id,
+            occupantGender: 'M'
+        },
+        {
+            number: '2022',
+            hallId: voltaHall._id,
+            occupantGender: 'F',
+        },
+        {
+            number: '2034',
+            hallId: voltaHall._id,
+            occupantGender: 'F'
+        },
+        {
+            number: 'D14',
+            hallId: commonwealthHall._id,
+            occupantGender: 'M'
+        },
+        {
+            number: 'E10',
+            hallId: commonwealthHall._id,
+            occupantGender: 'M'
+        }
+    ]
+
+    setupStudents(studentsData);
+    setupRooms(roomsData);
+
+    stop();
 })
